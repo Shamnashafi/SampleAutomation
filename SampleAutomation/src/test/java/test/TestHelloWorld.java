b348e0515768535865871703dcb0792bafbeed48
محمد شفن < @DESKTOP-9IRHC8N>
2022-11-01 16:46:14 +0400

    First Commit.

---
+++ SampleAutomation/src/test/java/test/TestHelloWorld.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.Test;
+
+public class TestHelloWorld {
+
+	@Test
+	public void secondTest()
+	{
+		System.out.println("Hello Its a second testing project");
+	}
+}
